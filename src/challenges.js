d8310e433bdd8e66d5c2f8d5d3377cfd687666ef
Tanise Santos <tanise234@gmail.com>
2022-03-11 15:10:50 -0300

    Desafio 1 concluído

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  //#vqv
+function compareTrue(bool1, bool2) {
+ if (bool1 === true && bool2 === true) {
+   return true;
+ }
+ else {
+   return false;
+ }
 }
 
 // Desafio 2
