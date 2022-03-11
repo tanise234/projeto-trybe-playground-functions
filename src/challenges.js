24231faf4da962324ec545a5eff393d546405ac7
Tanise Santos <tanise234@gmail.com>
2022-03-11 16:10:01 -0300

    Desafio 7  concluído

--- src/challenges.js
+++ src/challenges.js
@@ -52,8 +52,16 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 < cat2) {
+    return 'cat1';
+  }
+  else if (cat1 > cat2){
+    return 'cat2';
+  }
+  else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
