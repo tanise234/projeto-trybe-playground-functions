d7447957847e956dd3fe44f7c5f0a61fee580a4b
Tanise Santos <tanise234@gmail.com>
2022-03-11 15:37:18 -0300

    Desafio 4 concluído

--- src/challenges.js
+++ src/challenges.js
@@ -16,13 +16,12 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  // seu código aqui
   return string.split(' ');
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length -1] +', ' + array[0];
 }
 
 // Desafio 5
